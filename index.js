7f9c013f33945047e51083f9d1ee7bedba106bf9
ghetehri <gregoryh2000@gmail.com>
2018-11-13 14:33:51 -0500

    add admin code goo

--- index.js
+++ index.js
@@ -70,6 +70,71 @@ app.post('/database', function(req, res) {
   });
 });
 
+app.post('/email', function(req, res) {
+
+  var api_key = '73e812f6eef0659edade14aa0199a6df-9525e19d-fbd4c6ba';
+  var domain = 'sandbox247382c0af194dee852b5c74afeca012.mailgun.org';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+  var VarCode;
+
+  function makeId() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 4; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+
+  function sendEmail() {
+    var message = 'You have been invited to join the SOCS Reynolds Team. Varification Code: "' + VarCode + '"';
+
+    var data = {
+      from: 'Reynold SOCS Board <postmaster@sandbox247382c0af194dee852b5c74afeca012.mailgun.org>',
+      to: req.body.email,
+      subject: 'Admin Invitation',
+      text: message
+    };
+
+    console.log("Sent message: " + message + "  To: " + req.body.email);
+
+    /*mailgun.messages().send(data, function (error, body) {
+      console.log(body);
+    });*/
+  }
+
+  //check if varCode exists
+  MongoClient.connect(url, function(err, db) {
+    var exists;
+    var dbo = db.db("reynoldsdb");
+    do {
+      console.log("Generating Code..");
+      VarCode = makeId();
+      var query = { code: VarCode };
+      dbo.collection("verificationCodes").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        if (result == null || result == "") {
+          exists = false;
+        } else {
+          exists = true;
+        }
+        db.close();
+      });
+    } while (exists)
+
+    var codeObj = { code: VarCode, active: "t" };
+    dbo.collection("verificationCodes").insertOne(codeObj, function(err, res) {
+      if (err) throw err;
+      console.log("verification Code added");
+      sendEmail();
+      db.close();
+    });
+  });
+
+
+
+});
+
 http.listen(PORT, function(){
     console.log('listening on localhost:8080');
   });
